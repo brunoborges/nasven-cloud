11dc9a7fbd689c41c69c71c27988e0fde093f592
Bruno Borges <bruno.borges@gmail.com>
2015-11-03 04:07:56 -0800

    demo

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * Author: Bruno Borges (@brunoborges)
+ * Since: 2015
+ */
+var Spark = Java.type("spark.Spark");
+
+var books = new java.util.HashMap();
+
+Nasven.require("hack.js");
+Nasven.require("process.js");
+Nasven.require("routes.js");
+
+Nasven.daemon();
